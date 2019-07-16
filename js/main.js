9692a639078a5534a3ba98e12b227bc1679f5c65
Aleksi Tappura <atappura@gmail.com>
2019-07-16 08:58:29 +0300

    Swup options

--- js/main.js
+++ js/main.js
@@ -1,28 +1,10 @@
 $(document).ready(function() {
 
 	// Page transitions
-  let options = {
-    LINK_SELECTOR: 'a[href^="' + window.location.origin + '"]:not([data-no-swup]), a[href^="./"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])',
-    FORM_SELECTOR: 'form[data-swup-form]',
-    elements: [
-      '#swup'
-    ],
-    animationSelector: '[class*="transition-"]',
-    cache: true,
-    pageClassPrefix: '',
-    scroll: true,
-    debugMode: true,
-    preload: true,
-    support: true,
-    skipPopStateHandling: function (event) {
-      if (event.state && event.state.source == "swup") {
-        return false;
-      }
-      return true;
-    },
-    animateHistoryBrowsing: false,
-  }
-  const swup = new Swup(options);
+	let options = {
+		scroll: true,
+		animateHistoryBrowsing: false
+	}
 
 	const swup = new Swup(options);
 
